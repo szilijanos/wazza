80e778476b04e74c0f8af464a14583d757ae9461
Janos Szili <szili.jani@gmail.com>
2020-01-03 17:36:14 +0100

    first unit tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-expressions */
-import { html, fixture, expect } from '@open-wc/testing';
+import { fixture, expect } from '@open-wc/testing';
 
 import './app.js';
 
 describe('App component', () => {
-    it('should exist', async () => {
-        const el = (await fixture('<my-schedules-app></my-schedules-app>'));
-        expect(el).to.exist;
+  let element;
+
+  beforeEach(async () => {
+    element = await fixture('<my-schedules-app></my-schedules-app>');
+  });
+
+  [
+    'app-header',
+    'app-footer',
+    'main',
+    // Warning: the next ones will change, as the implementation progresses
+    // TODO: remove this comment once void
+    'app-schedules',
+  ].forEach(innerElementTag => {
+    it(`should render a <${innerElementTag}> in the shadow dom of the app element`, () => {
+      expect(element.shadowRoot.querySelector(innerElementTag)).to.exist;
     });
+  });
 });
